fde51c07993a531887f882390a137e20920b50fe
Avumile <smithavery435@gmail.com>
2023-12-17 01:01:06 -0800

    insertion sort update

--- 1-insertion_sort_list.c
+++ 1-insertion_sort_list.c
@@ -2,7 +2,6 @@
 #include <stdlib.h>
 #include "sort.h"
 
-void insertion_sort_list(listint_t **list);
 /**
  * insertion_sort_list - Sorts a doubly linked list in ascending order using
  *                       the insertion sort algorithm.
@@ -10,54 +9,52 @@ void insertion_sort_list(listint_t **list);
  */
 void insertion_sort_list(listint_t **list)
 {
-	listint_t *current, *back, *next_node, *temp;
-	int swap_count;
+    listint_t *current, *back, *temp, *next_node;
 
-	if (list == NULL || *list == NULL || (*list)->next == NULL)
-		return;
+    if (list == NULL || *list == NULL || (*list)->next == NULL)
+        return;
 
-	current = (*list)->next;
+    current = (*list)->next;
 
-	while (current)
-	{
-		swap_count = 0; /* Initialize swap count for each outer loop iteration */
+    while (current)
+    {
+        int swap_count = 0; /* Initialize swap count for each outer loop iteration */
+        back = current->prev;
+        next_node = current->next;
 
-		back = current->prev;
-		next_node = current->next;
+        while (back && back->n > current->n)
+        {
+            back->next = next_node;
 
-		while (back && back->n > current->n)
-		{
-			back->next = next_node;
+            if (next_node != NULL)
+                next_node->prev = back;
 
-			if (next_node != NULL)
-				next_node->prev = back;
+            current->next = back;
+            current->prev = back->prev;
 
-			current->next = back;
-			current->prev = back->prev;
+            if (back->prev != NULL)
+                back->prev->next = current;
+            else
+                *list = current; /* Update the head if needed */
 
-			if (back->prev != NULL)
-				back->prev->next = current;
-			else
-				*list = current; /* Update the head if needed */
+            back->prev = current;
 
-			back->prev = current;
+            /* Correct the next_node pointer */
+            temp = back;
+            back = current->prev;
+            next_node = temp;
 
-			/* Correct the next_node pointer */
-			temp = back;
-			back = current->prev;
-			next_node = temp;
+            /* Traverse result list and compare every node with our current node. */
+            swap_count++;
+        }
 
-			/* Traverse result list and compare every node with our current node. */
-			swap_count++;
-		}
+        /* If any swaps, print the list after the inner loop */
+        if (swap_count > 0)
+        {
+            print_list(*list);
+        }
 
-		/* If any swaps , print the list after the inner loop */
-		if (swap_count > 0)
-		{
-			print_list(*list);
-		}
-
-		current = next_node; /* Move to the next element in the list */
-	}
+        current = next_node; /* Move to the next element in the list */
+    }
 }
 
