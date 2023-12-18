462b12ac58a808fbb8b7e847b6dbc151e5607b44
Tau <taumasaire@gmail.com>
2023-12-18 10:47:51 +0200

    updating the header file

--- sort.h
+++ sort.h
@@ -28,5 +28,6 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
+void shell_sort(int *array, size_t size);
 #endif /* MAIN_H */
 
