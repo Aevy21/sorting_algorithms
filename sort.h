f33d98799c2f6c56363ee3410d7f8e95a5e82067
Avumile <smithavery435@gmail.com>
2023-12-18 08:07:16 -0800

    merge sort algorithm  function

--- sort.h
+++ sort.h
@@ -28,5 +28,8 @@ void selection_sort(int *array, size_t size);
 void quick_sort(int *array, size_t size);
 void swap_nodes(listint_t **list, listint_t *node1, listint_t *node2);
 void cocktail_sort_list(listint_t **list);
+void merge(int *array, size_t left_size, size_t right_size);
+void merge_sort(int *array, size_t size);
+
 #endif /* MAIN_H */
 
